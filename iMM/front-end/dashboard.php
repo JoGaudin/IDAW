712522eb062eb92ea38bfbdeedb762798c0b5bfc
alexandrefavreau7 <101090637+alexandrefavreau7@users.noreply.github.com>
2022-04-04 00:51:48 +0200

    Create dashboard.php

---
+++ iMM/front-end/dashboard.php
@@ -0,0 +1,95 @@
+<?php
+require_once('template_header.php');
+require_once('template_menu.php');
+
+
+
+session_start();
+
+
+?>
+
+<script>
+
+function graphe(libelle, ratios){
+   
+    var data = [{
+
+  values : ratios,
+  labels :libelle,
+
+
+  type: 'pie',
+
+  
+
+
+}];
+
+var layout = {
+  height: 400,
+  width: 500
+
+};
+Plotly.newPlot('myDiv', data, layout);
+}
+
+
+
+</script>
+
+<script>
+
+     libelle = new Array ();
+     ratios = new Array ();
+$(document).ready( function () {
+    $.ajax({
+    type: 'POST',
+    data : 'login=<?php echo $_SESSION['LOGIN']?>',
+    url: '../back-end/dashboard.php',
+    Type: 'JSON',
+    }).done(function(data) {
+    
+    data = JSON.parse(data);
+    
+    
+
+    i = 0;
+    
+    data.forEach(row => {
+        // alert (row[0]);
+        libelle[i]= row[0];
+        ratios[i] = row[1];
+        i++;
+        
+
+                   });
+
+graphe(libelle, ratios);
+//                    var data = [{
+//   values: libelle,
+//   labels: ratios,
+//   type: 'pie'
+// }];
+
+// var layout = {
+//   height: 400,
+//   width: 500
+// };
+
+
+// Plotly.newPlot('myDiv', data, layout);
+
+    })
+
+});
+
+</script>
+
+<script src='js/plotly-2.9.0.min.js'></script>
+<div id='myDiv' ></div>
+
+
+
+
+
